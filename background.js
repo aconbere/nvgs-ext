34aa583974fbca11f9b366adef01a7e2769faa04
Anders Conbere <anders@conbere.org>
2025-03-13 17:57:12 -0700

    working

--- background.js
+++ background.js
@@ -1,5 +1,47 @@
-let addCrawl = (url) => {
-    let result = fetch("http://localhost:3000/crawls", {
+let ADD_TITLE = "add site";
+let REMOVE_TITLE = "remove site";
+
+let setTriangleState = async (tabId) => {
+    await browser.action.setTitle({
+        title: ADD_TITLE,
+        tabId: tabId,
+    });
+    await browser.action.setIcon({
+        path: "icons/triangle.svg",
+        tabId: tabId,
+    })
+}
+
+let setCircleState = async (tabId) => {
+    await browser.action.setTitle({
+        title: REMOVE_TITLE,
+        tabId: tabId,
+    });
+    await browser.action.setIcon({
+        path: "icons/circle.svg",
+        tabId: tabId,
+    })
+}
+
+let isTriangle = async (tabId) => {
+    let title = await browser.action.getTitle({
+        tabId: tabId
+    });
+
+    return title === ADD_TITLE;
+}
+
+
+let isOk = (statusCode) => {
+    if (statusCode >= 200 && statusCode < 300) {
+        return true;
+    }
+    return false;
+}
+
+let addCrawl = async (url) => {
+    console.log(`adding: ${url}`);
+    return await fetch("http://localhost:3000/crawls", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -8,11 +50,13 @@ let addCrawl = (url) => {
             urls: [ url ]
         }),
     });
+
 }; 
 
-let removeCrawl = (url) => {
-    let result = fetch("http://localhost:3000/crawls", {
-        method: "DELETE",
+let removeCrawl = async (url) => {
+    console.log(`removing: ${url}`);
+    return await fetch("http://localhost:3000/crawls/delete", {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
@@ -22,8 +66,14 @@ let removeCrawl = (url) => {
     });
 }; 
 
-let getCrawn = (url) => {
-    let result = fetch("http://localhost:3000/crawls/get", {
+/* returns a "Crawl"
+ * { "url": String,
+ *   "last_fetched_at": String,
+ *   }
+ */
+let getCrawl = async (url) => {
+    console.log(`fetching: ${url}`);
+    let response = await fetch("http://localhost:3000/crawls/get", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -32,16 +82,62 @@ let getCrawn = (url) => {
             url: url
         }),
     });
+
+    if (!isOk(response.status)) {
+        return null;
+    }
+
+    return await response.json();
 }; 
 
 
 let onClick = () => {
     browser.tabs
         .query({currentWindow: true, active: true})
-        .then((tabs) => {
-            console.log("tabs", tabs);
-            addSite(tabs[0].url);
+        .then(async (tabs) => {
+            let tabId = tabs[0].id; 
+            let url = tabs[0].url;
+
+            if (await isTriangle(tabId)) {
+                let response = await addCrawl(url);
+
+                if (isOk(response.status)) {
+                    await setCircleState(tabId);
+                }
+            } else {
+                let response = await removeCrawl(url);
+
+                if (isOk(response.status)) {
+                    await setTriangleState(tabId);
+                }
+            }
         })
 }
 
+let onTabUpdated = async (tabId, changeInfo, tab) => {
+    if (changeInfo["status"] !== "complete") {
+        return;
+    }
+
+    let url = tab["url"];
+
+    if (url.startsWith("about:")) {
+        await browser.action.disable();
+        return;
+    }
+
+    let crawl = await getCrawl(url);
+
+    if (crawl === null) {
+        await setTriangleState(tabId);
+        return;
+    }
+    await setCircleState(tabId);
+};
+
+//let onStartup = (e) => {
+//};
+//
+//browser.runtime.onStartup(onStartup);
 browser.action.onClicked.addListener(onClick);
+browser.tabs.onUpdated.addListener(onTabUpdated, { properties: ["status"] });
